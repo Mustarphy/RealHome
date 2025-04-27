8314b2cdaae2756ed2ac409f739291e883de9851
Mustarphy <abdulazeezmustapha204@gmail.com>
2025-04-27 05:58:04 -0700

    setSocket link  commit

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const apiRequest = axios.create({
-    baseURL: 'http://localhost:8800/api',
+    baseURL: 'https://kingshomeapi-1.onrender.com',
     withCredentials: true,
 });
 
